72a2d96277a0070dbf91edd344835c99b18c11c2
Nooh KVM <nooh64@gmail.com>
2016-06-02 15:46:28 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -46,7 +46,7 @@ fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add hello.txt
+git add ./hello.txt
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
